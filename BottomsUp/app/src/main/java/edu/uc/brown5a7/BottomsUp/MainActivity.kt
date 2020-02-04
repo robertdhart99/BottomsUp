a9451f2d34c7043f5f4bcadbce23333b6b2604b4
brownadam010 <45324454+brownadam010@users.noreply.github.com>
2020-02-03 19:53:26 -0500

    Add files via upload

---
+++ BottomsUp/app/src/main/java/edu/uc/brown5a7/BottomsUp/MainActivity.kt
@@ -0,0 +1,19 @@
+package edu.uc.brown5a7.BottomsUp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.uc.brown5a7.BottomsUp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
