f21666626957688df5a3ed25e568240e83c4e208
Adam Brown <brownadam010@gmail.com>
2020-02-25 20:25:14 -0500

    All Unit Tests pass for Retrofit

--- BottomsUp/app/src/main/java/edu/uc/brown5a7/BottomsUp/RetrofitClientInstance.kt
+++ BottomsUp/app/src/main/java/edu/uc/brown5a7/BottomsUp/RetrofitClientInstance.kt
@@ -1,4 +1,22 @@
 package edu.uc.brown5a7.BottomsUp
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClientInstance {
+
+    private var retrofit: Retrofit? = null
+    private val BASE_URL = "https://bottomsup-f3e22.firebaseio.com"
+
+    val retrofitInstance : Retrofit?
+        get(){
+            // has this drink been created yet
+            if(retrofit == null){
+                retrofit = retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
 }
--- BottomsUp/app/src/main/java/edu/uc/brown5a7/BottomsUp/dto/Drink.kt
+++ BottomsUp/app/src/main/java/edu/uc/brown5a7/BottomsUp/dto/Drink.kt
@@ -2,7 +2,7 @@ package edu.uc.brown5a7.BottomsUp.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Drink(@SerializedName("id") var drinkId: Int = 0, var category: String, var brand: String, var name: String, var comment: String, var rating: Int) {
+data class Drink(@SerializedName("id") var drinkId: String = "0", var category: String, var brand: String, var name: String, var comment: String, var rating: String) {
 
     override fun toString(): String{
         return "$category $brand $name $comment $rating"
