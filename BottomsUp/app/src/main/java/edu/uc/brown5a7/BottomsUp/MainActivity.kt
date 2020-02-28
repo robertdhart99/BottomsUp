b8ecb832db2b751c997f82e7bc901204cc9016f7
Masha <mneshenko@gmail.com>
2020-02-28 16:32:11 -0500

    adapter?

--- BottomsUp/app/src/main/java/edu/uc/brown5a7/BottomsUp/MainActivity.kt
+++ BottomsUp/app/src/main/java/edu/uc/brown5a7/BottomsUp/MainActivity.kt
@@ -2,20 +2,36 @@ package edu.uc.brown5a7.BottomsUp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import edu.uc.brown5a7.BottomsUp.searchAdapter.DrinkAdapter
 import edu.uc.brown5a7.BottomsUp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow();
+        setContentView(R.layout.main_fragment)
 
-            //test 
-        }
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = DrinkAdapter(drinkList)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
     }
 
 }
