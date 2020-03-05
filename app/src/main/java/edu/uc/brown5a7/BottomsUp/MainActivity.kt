29a12af1f4c0f219d80b2afc1d718cc7188ea4aa
robertdhart99 <47120052+robertdhart99@users.noreply.github.com>
2020-03-05 18:59:44 -0500

    opmopq3gmopgm

---
+++ app/src/main/java/edu/uc/brown5a7/BottomsUp/MainActivity.kt
@@ -0,0 +1,38 @@
+package edu.uc.brown5a7.BottomsUp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuInflater
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import edu.uc.brown5a7.BottomsUp.dto.Drink
+import edu.uc.brown5a7.BottomsUp.searchAdaptor.drinkAdapter
+import edu.uc.brown5a7.BottomsUp.service.DrinkService
+//import edu.uc.brown5a7.BottomsUp.searchAdapter.DrinkAdapter
+import edu.uc.brown5a7.BottomsUp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_fragment)
+
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        var drinks: MutableLiveData<ArrayList<Drink>> = MutableLiveData()
+        var drinkService: DrinkService = DrinkService()
+        drinks = drinkService.fetchDrinks("blank")
+        val drinksFullList: ArrayList<Drink> = drinks as ArrayList<Drink>
+        val adapter = drinkAdapter(this, drinksFullList)
+        recyclerView.adapter = adapter
+
+
+    }
+}
