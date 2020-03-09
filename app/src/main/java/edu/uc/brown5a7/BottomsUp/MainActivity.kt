b327c8c70b4e896fbf5e80b16f1793469ec7a368
Jasrah85 <alainna.nichols85@gmail.com>
2020-03-08 23:30:32 -0400

    My code review suggestions.

--- app/src/main/java/edu/uc/brown5a7/BottomsUp/MainActivity.kt
+++ app/src/main/java/edu/uc/brown5a7/BottomsUp/MainActivity.kt
@@ -2,37 +2,39 @@ package edu.uc.brown5a7.BottomsUp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuInflater
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import edu.uc.brown5a7.BottomsUp.dto.Drink
-import edu.uc.brown5a7.BottomsUp.searchAdaptor.drinkAdapter
+import edu.uc.brown5a7.BottomsUp.searchAdaptor.DrinkAdapter
 import edu.uc.brown5a7.BottomsUp.service.DrinkService
+import kotlinx.android.synthetic.main.main_fragment.*
+
 //import edu.uc.brown5a7.BottomsUp.searchAdapter.DrinkAdapter
-import edu.uc.brown5a7.BottomsUp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var viewManager: LayoutManager
+    private lateinit var drinkAdapter: DrinkAdapter
+    private var drinks: MutableLiveData<ArrayList<Drink>> = MutableLiveData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_fragment)
 
-        val layoutManager = LinearLayoutManager(this)
-        layoutManager.orientation = LinearLayoutManager.VERTICAL
-        recyclerView.layoutManager = layoutManager
+        initRecyclerView()
+        //UNCOMMENT THESE LINES WHEN CASTING HAS BEEN WORKED OUT FOR drinksFullList:
+        /*val drinksFullList: ArrayList<Drink> = drinks as ArrayList<Drink> //ClassCastException: androidx.lifecycle.MutableLiveData cannot be cast to java.util.ArrayList
+        drinkAdapter = DrinkAdapter(this, drinksFullList)
+        recycler_view.adapter = drinkAdapter*/
+    }
 
-        var drinks: MutableLiveData<ArrayList<Drink>> = MutableLiveData()
-        var drinkService: DrinkService = DrinkService()
+    private fun initRecyclerView() {
+        val drinkService = DrinkService()
         drinks = drinkService.fetchDrinks("blank")
-        val drinksFullList: ArrayList<Drink> = drinks as ArrayList<Drink>
-        val adapter = drinkAdapter(this, drinksFullList)
-        recyclerView.adapter = adapter
-
-
+        recycler_view.layoutManager = LinearLayoutManager(this@MainActivity)
     }
 }
