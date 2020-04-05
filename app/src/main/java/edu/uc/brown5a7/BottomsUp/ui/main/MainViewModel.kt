4119b47a966fc05fee219d576192321609c81cac
rosasmn <rosasmn@mail.uc.edu>
2020-04-05 10:36:35 -0400

    Modifying MainViewModel.kt; Modifying fetchCategory function to reference categories variable and category service variable from the newly created DrinkCategoryService

--- app/src/main/java/edu/uc/brown5a7/BottomsUp/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/brown5a7/BottomsUp/ui/main/MainViewModel.kt
@@ -4,17 +4,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import edu.uc.brown5a7.BottomsUp.dto.Drink
 import edu.uc.brown5a7.BottomsUp.service.DrinkService
+import edu.uc.brown5a7.BottomsUp.service.DrinkCategoryService
 
 class MainViewModel : ViewModel() {
     var drinks: MutableLiveData<ArrayList<Drink>> = MutableLiveData()
     var drinkService: DrinkService = DrinkService()
+    var categories: MutableLiveData<ArrayList<Drink>> = MutableLiveData()
+    var drinkCategoryService : DrinkCategoryService = DrinkCategoryService()
 
     fun fetchDrinks(name: String) {
         drinks = drinkService.fetchDrinks(name)
     }
 
     fun fetchCategory(category: String) {
-        drinks = drinkService.fetchCategory(category)
+        categories = drinkCategoryService.fetchCategory(category)
     }
 
 
