9b0b258271ad618f9a4d5e6c2704cb2c97fe2fec
Masha <mneshenko@gmail.com>
2020-04-24 12:43:45 -0400

    broken the nav down

--- app/src/main/java/edu/uc/brown5a7/BottomsUp/MainActivity.kt
+++ app/src/main/java/edu/uc/brown5a7/BottomsUp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.MenuInflater
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.uc.brown5a7.BottomsUp.dto.Drink
 import edu.uc.brown5a7.BottomsUp.searchAdaptor.drinkAdapter
 import edu.uc.brown5a7.BottomsUp.service.DrinkService
@@ -28,6 +29,23 @@ class MainActivity : AppCompatActivity() {
         val adapter = drinkAdapter(this, drinksFullList)
         recyclerView.adapter = adapter
 
-
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_recents:
+                    Toast.makeText(MainActivity.this, "Recents", Toast.LENGTH_SHORT).show();
+                    break;
+                    case R.id.action_favorites:
+                    Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                    break;
+                    case R.id.action_nearby:
+                    Toast.makeText(MainActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
+                    break;                }
+                return true;
+            }
+        });
+    }
     }
 }
